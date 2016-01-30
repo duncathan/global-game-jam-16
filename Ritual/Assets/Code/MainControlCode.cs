23c6bbc9220f11bc1a9501db35bb9331e3bd80dd
gemohandy <bennetteidsness@gmail.com>
2016-01-30 09:35:56 -0600

    Main Menu
    Added a main menu, as well as developing the basic infrastructure to
    play the music, save the level and all that stuff. It’s not done yet,
    but it will be much easier now.

--- Ritual/Assets/Code/MainControlCode.cs
+++ Ritual/Assets/Code/MainControlCode.cs
@@ -6,10 +6,15 @@ public class MainControlCode : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
 		DontDestroyOnLoad (transform.gameObject);
+		if (FindObjectsOfType<MainControlCode> () [1]!= null) {
+			DestroyObject(gameObject);
+		}
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+		if (Input.GetKey (KeyCode.Backspace)) {
+			Application.LoadLevel(0);
+		}
 	}
 }
