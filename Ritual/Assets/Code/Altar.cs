9a33cb3cf65ce752b42d6ec40adaab1bcbbc6819
gemohandy <bennetteidsness@gmail.com>
2016-01-30 12:11:58 -0600

    Second Level
    The second level has been added, and the code to switch to it has been
    implemented.

---
+++ Ritual/Assets/Code/Altar.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Altar : MonoBehaviour {
+
+	GameObject target;
+	Collider2D[] touchers; 
+
+	void Start() {
+		target = GameObject.Find ("MainSphere");
+	}
+
+	void Update(){
+		touchers = Physics2D.OverlapAreaAll (new Vector2 (-1, -4.25f), new Vector2 (1, -3.75f));
+		foreach (Collider2D col in touchers) {
+			if(col.gameObject==target.gameObject){
+				Application.LoadLevel(Application.loadedLevel+1);
+			}
+		}
+	}
+}
